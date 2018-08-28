f7683f0ba059235c01cc092f469c792c7042c5a8
Dewi Syarah Sofiati <42134623+dewissofiati@users.noreply.github.com>
2018-08-28 07:11:48 +0700

    program java akses

---
+++ aksestingkatkelas.java
@@ -0,0 +1,6 @@
+public class aksestingkatkelas{
+    //ada dua nilai, yaitu default dan public
+    //ketika menggunakan default (jika dalam penulisan boleh tidak ditulis, contoh class. itu berarti dia default)
+    //maka kelas tersebut tidak dapat diimport ke kelas dengan package yang berbeda
+    //untuk itu digunakan public untuk dapat meng import ke kelas dengan package berbeda
+}
---
+++ aksestingkatmember.java
@@ -0,0 +1,23 @@
+class aksestingkatmember{
+    //member itu ada 2, data seperti biasa dan metode
+    /*contoh member
+    int test = 5;
+    
+    int get_test(){
+        return test;*/
+
+    /*di member ada 4 nilai akses tingkat yaitu 
+    1. default
+    2. public
+    3. protected
+    4. private
+    
+    default dan public masih sama dengan pada kelas sebelumnya
+    untuk protected ada 2 kondisi
+    pertama, ketika import pada package yg sama masih bisa mengakses
+    kedua, ketika import pada package yg berbeda bisa mengakses dengan cara dia adalah subkelas (yg memakai extends)
+    atau dia adalah pewaris dari kelas yg pertama jadi syarat kedua dia adalah pewarisnys
+    
+    untuk private, dia tidak dapat di import didalam package yg sama maupun berbeda
+    privat hanya dapat digunakan pada kelas yg sama atau hanya pada kelas itu sendiri*/
+}
